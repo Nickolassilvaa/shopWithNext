a3cb13a40f12273a0106ae7b0419d3ba1cbd62d7
nrp-silva <nickolascaragua@gmail.com>
2022-11-17 23:19:22 -0300

    first commit

---
+++ src/styles/index.ts
@@ -0,0 +1,18 @@
+import { createStitches } from '@stitches/react'
+
+export const { 
+  config, 
+  styled,
+  globalCss,
+  createTheme,
+  css,
+  getCssText,
+  keyframes,
+  theme
+} = createStitches({
+  theme: {
+    colors: {
+      rocketseat: '#8257e6'
+    }
+  }
+})
